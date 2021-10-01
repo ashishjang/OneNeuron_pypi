3d2d8106d69bb3aeb558a8373562aa40945124e9
Ashish <ashishjang@outlook.com>
2021-10-01 18:38:15 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,20 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+PROJECT_NAME = "Perceptron"
+USER_NAME = "ashishjang"
+
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",
+    name= f"{PROJECT_NAME}-{USER_NAME}",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author=USER_NAME,
+    author_email="astroed44@gmail.com",
+    description="An implimentation of perceptron.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ashishjang/OneNeuron_pypi",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/ashishjang/OneNeuron_pypi/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -22,5 +25,12 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pandas",
+        "seaborn",
+        "tqdm"
+    ]
 )
